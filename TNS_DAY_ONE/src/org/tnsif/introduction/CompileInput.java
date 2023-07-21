20ac0e5f5ff4851dec2f5d4ab2173d058d72ee45
Avinash Lone <124510682+avinashlone22@users.noreply.github.com>
2023-07-21 12:44:02 +0530

    first commit

---
+++ TNS_DAY_ONE/src/org/tnsif/introduction/CompileInput.java
@@ -0,0 +1,13 @@
+package org.tnsif.introduction;
+
+public class CompileInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		short num =34;
+		System.out.println("the num is "+num);
+	}
+
+}
